27037d2072c60dedd3085d191dfe7aa4c2ebca5e
sflaming <symonflaming@gmail.com>
2023-08-08 00:37:07 -0400

    add args

--- get_hot_images.py
+++ get_hot_images.py
@@ -3,6 +3,8 @@
 import os
 import requests
 import sys
+import argparse
+import shutil
 
 
 def download_image(url, destination_dir):
@@ -22,14 +24,7 @@ def download_image(url, destination_dir):
         print(f"Skipped (Already Exists): {url}")
 
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python3 get_hot_images.py <destination_dir> <subreddit1,subreddit2,...>")
-        sys.exit(1)
-
-    destination_dir = sys.argv[1]
-    subreddits = sys.argv[2].split(',')
-
+def fetch_hot_images(subreddits, destination_dir):
     for subreddit in subreddits:
         # Fetch the top 15 hot posts from the subreddit using Reddit API
         url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=15"
@@ -45,12 +40,35 @@ def main():
                     if "data" in post and "url" in post["data"] and post["data"]["url"].endswith(('.jpg', '.png', '.jpeg', '.gif')):
                         download_image(post["data"]["url"], destination_dir)
             else:
-                print(f"Failed to retrieve data from /r/{subreddit}.")
-
+                print(f"Failed to retrieve data from subreddit: {subreddit}")
         except requests.exceptions.RequestException as re:
-            print(f"API Request Error for /r/{subreddit}: {re}")
+            print(f"API Request Error for subreddit {subreddit}: {re}")
         except Exception as e:
-            print(f"Unexpected Error for /r/{subreddit}: {e}")
+            print(f"Unexpected Error for subreddit {subreddit}: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Fetch hot images from subreddits.")
+    parser.add_argument("-s", "--subreddits", nargs="+", default=["fujifilm"], help="Subreddit name(s) (default: fujifilm)")
+    parser.add_argument("-d", "--destination", default="~/Pictures/RedditScreensaver", help="Destination path (default: ~/Pictures/RedditScreensaver)")
+    parser.add_argument("-c", "--clear", action="store_true", help="Clear the folder before fetching new images")
+
+    args = parser.parse_args()
+
+    # Resolve the destination path
+    destination_dir = os.path.expanduser(args.destination)
+
+    # Clear the folder if the --clear option is provided
+    if args.clear:
+        try:
+            shutil.rmtree(destination_dir)
+        except FileNotFoundError:
+            pass  # The folder doesn't exist yet
+
+    # Create the destination directory if it doesn't exist
+    os.makedirs(destination_dir, exist_ok=True)
+
+    # Fetch hot images from the specified subreddits
+    fetch_hot_images(args.subreddits, destination_dir)
 
 
 if __name__ == "__main__":
